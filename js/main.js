b6a97a65df3d08bd3205f93c3d93513795ab3587
Derek Neuts <dcneuts@gmail.com>
2017-07-02 21:45:04 -0700

    Setup transition by using JS object literal to define in and out movements for loading pages.

--- js/main.js
+++ js/main.js
@@ -2,4 +2,7 @@
  * Created by derek on 7/2/17.
  */
 // Must use jQuery versus vanilla JS per plugin docs
-$(".animsition").animsition();
+$(".animsition").animsition({
+	inClass: 'fade-in-right-lg',
+	outClass: 'fade-out-right-lg'
+});
