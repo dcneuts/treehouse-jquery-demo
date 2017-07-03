392f3b82cd56df9f02b297796d0730f81a2d0add
Derek Neuts <dcneuts@gmail.com>
2017-07-03 00:00:22 -0700

    Setting up files for further customization in the main.js file with stickyjs plugin

--- js/main.js
+++ js/main.js
@@ -2,11 +2,21 @@
  * Created by derek on 7/2/17.
  */
 // Must use jQuery versus vanilla JS per plugin docs
-$(".animsition").animsition({
+$('.animsition').animsition({
 	inClass: 'fade-in-right-lg',
 	outClass: 'fade-out-right-lg',
 	linkElement: 'header a',
 	inDuration: 1000,
 	outDuration: 500
 });
+
+// Customization for StickyJS plugin
 $('.header').sticky();
+
+$('.header').on('sticky-start', function() {
+	$('.description').html('We build <strong>great</strong> apps');
+});
+
+$('.header').on('sticky-end', function() {
+	$('.description').html('We build apps');
+});
